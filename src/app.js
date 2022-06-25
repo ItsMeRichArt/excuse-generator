b1da73157414515bcfa5c3e20376c51e710d9d22
Richard Orellana <richard.o2188@gmail.com>
2022-06-25 18:04:51 +0000

    Excuse generator exercise

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let item = ["my computer", "my homework", "my saxophone", "my car"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function onLoad(whoArray, whatArray, itemArray, whenArray) {
+  let randomWho = whoArray[Math.floor(Math.random() * whoArray.length)];
+  let randomWhat = whatArray[Math.floor(Math.random() * whatArray.length)];
+  let randomItem = itemArray[Math.floor(Math.random() * itemArray.length)];
+  let randomWhen = whenArray[Math.floor(Math.random() * whenArray.length)];
+
+  let answer =
+    "OMG! I'm so sorry... but " +
+    randomWho +
+    " " +
+    randomWhat +
+    " " +
+    randomItem +
+    " " +
+    randomWhen +
+    " ";
+
+  return answer;
+}
+document.getElementById("excuse").innerHTML = onLoad(who, what, item, when);
